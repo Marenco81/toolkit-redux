4abff0ed60870f0bafce87aab70d0fa68bd4b588
Alvaro Marenco <alvaromarencom@gmail.com>
2024-12-03 15:12:36 -0600

    Usando custom hook useGetTodosQuery

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,12 +1,25 @@
+import { useGetTodosQuery } from "./store/apis/todosApi"
 
 export const TodoApp = () => {
+
+  const { data = [] , isLoading } = useGetTodosQuery();
+
   return (
     <>
       <h1>Todo - RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? 'True' : 'False' } </h4>
 
       <pre>...</pre>
+
+      <ul>
+        {data.map(todo => (
+          <li key={todo.id}>
+              <strong> {todo.completed ? 'Done' : 'Pending' } </strong>
+            {todo.title}
+          </li>
+        ) )}
+      </ul>
 
       <button>
         Next Todo
